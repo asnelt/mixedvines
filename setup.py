72b2893806367784949483c4c7d581bbc8554990
Arno Onken <arno.onken@iit.it>
2017-04-23 22:03:05 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2017 Arno Onken
+#
+# This file is part of the mixedvines package.
+#
+# The mixedvines package is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# The mixedvines package is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+Setup for the mixedvines package.
+'''
+from setuptools import setup
+
+setup(
+    name="mixedvines",
+    version="1.0",
+    description=("Package for canonical vine copula trees with mixed"
+                 " continuous and discrete marginals."),
+    long_description=("This packge contains a complete framework based on"
+                      " canonical vine copulas for modelling multivariate data"
+                      " that are partly discrete and partly continuous.  The"
+                      " resulting multivariate distributions are flexible with"
+                      " rich dependence structures and arbitrary marginals."),
+    keywords="copula mixed vine continuous dicrete entropy",
+    url="https://github.com/asnelt/mixedvines/",
+    author="Arno Onken",
+    author_email="asnelt@asnelt.org",
+    license="GPLv3+",
+    packages=["mixedvines"],
+    install_requires=["scipy"],
+    test_suite="nose2.collector.collector",
+    tests_require=["nose2"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        ("License :: OSI Approved :: GNU General Public License v3 or later"
+        " (GPLv3+)"),
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering"]
+)
